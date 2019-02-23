6564a30a1d2b5961cf603a67551195822f9cb0e6
Kiyo Matsui <kiyo.matsui@aecom.com>
2019-02-23 20:18:55 +0000

    for v0.01

--- experiments/06NoCacheLinePingPong.cpp
+++ experiments/06NoCacheLinePingPong.cpp
@@ -10,9 +10,10 @@ struct data {
   std::atomic_bool flag1 = false;
   std::atomic_bool flag2 = false;  
   std::array<int, 5> a = {{1, 2, 3, 4, 5}};
+  std::array<int, 5> a2 = {{1, 2, 3, 4, 5}};
   char b[1000000]; // large array so probably no longer on same cache line.
-  //char b[1]; // i.e. all data on same cache line
   std::array<int, 5> c = {{1, 2, 3, 4, 5}};
+  std::array<int, 5> c2 = {{1, 2, 3, 4, 5}};
 };
 
 int threadA(data* aData) {
@@ -24,6 +25,9 @@ int threadA(data* aData) {
     for (auto& m : aData->c) {
       m++;
     }
+    for (auto& m : aData->c2) {
+      m++;
+    }    
   } 
   return 0;
 }
@@ -43,10 +47,13 @@ int main() {
     for (auto& m : x.a) {
       m++;
     }
+    for (auto& m : x.a2) {
+      m++;
+    }    
   } 
   t1.join();
   auto end = std::chrono::high_resolution_clock::now();  
-  std::cout << "03FalseSharingTest runtime "
+  std::cout << "04NoFalseSharingTest runtime "
             << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()
             << " microseconds\n";
   return 0;
